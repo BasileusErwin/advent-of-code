23ed03e649685e06e1077002a73409722da6079f
Ignacio Perez <ignacio.perez2123@protonmail.com>
2023-12-02 08:38:59 -0300

    Day 2 AoC: Cube Conundrum Part 2
    - Implemented the function 'calculatePowerOfSet' to calculate the power of an individual set.
      This function determines the maximum number of cubes of each color in the game sets and calculates their product.
    - Added the function 'calculatePowerOfGames' which sums the power of all the sets in the provided sequence, thus providing the total combined power.
    - These functions allow completing the second part of the Day 2 challenge,
      providing an efficient and straightforward solution to calculate the power of the games based on the cube constraints.

--- 2023/day_2/src/main.nim
+++ 2023/day_2/src/main.nim
@@ -82,11 +82,47 @@ proc getTotalGamesIsPossible(games: seq[Game]): int =
 
 # Part 2
 
-proc calculatePowerOfCube(cube: Cube): int =
-  return 0
+# calculatePowerOfSet: Calculates the "power" of an individual set.
+# The power is defined as the product of the maximum number of red, green and blue cubes used in any set in the set.
+# used in any set in the game. This function determines the maximum number of cubes
+# of each color required in the sets of the game and returns its product.
+#
+# Parameters:
+#
+# game: a Game object containing the sets of cubes to evaluate.
+#
+# Returns: The power of the game as an integer.
+proc calculatePowerOfSet(game: Game): int =
+  var
+    red: int = 0
+    green: int = 0
+    blue: int = 0
 
+  for set in game.sets:
+    if set.red > red:
+      red = set.red
+    if set.green > green:
+      green = set.green
+    if set.blue > blue:
+      blue = set.blue
+
+  return red * green * blue
+
+# calculatePowerOfGames: Calculates the total combined power of a sequence of games.
+# Iterates over each game, calculating its individual power with the function calculatePowerOfSet
+# and adding these powers to get the total.
+#
+# Parameters:
+# games: a sequence of Game objects to evaluate.
+#
+# Returns: The combined total power of all games as an integer.
 proc calculatePowerOfGames(games: seq[Game]): int =
-  return 0
+  var power: int = 0
+
+  for game in games:
+    power += calculatePowerOfSet(game)
+
+  return power
 
 when isMainModule:
   let games = loadFileAndPrecess()
